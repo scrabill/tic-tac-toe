817869c224e34678798de0078c1e1cc33f51d666
scrabill <shannon.crabill@gmail.com>
2020-04-13 15:51:27 -0400

    Adding time travel

--- src/index.js
+++ src/index.js
@@ -72,14 +72,15 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
-      xIsNext: true
+      xIsNext: true,
+      stepNumber: 0
     }
   }
 
   // Concat does not mutate the original array like .push does
   handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length - 1]
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const current = history[this.state.stepNumber]
     const squares = current.squares.slice();
 
     if (calculateWinner(squares) || squares[i]) {
@@ -90,8 +91,16 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
   }
 
   render() {
