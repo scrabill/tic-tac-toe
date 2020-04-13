f4d829fcf78fda91c05b698881c72b65e58ac931
scrabill <shannon.crabill@gmail.com>
2020-04-13 15:03:56 -0400

    Lifting state up

--- src/index.js
+++ src/index.js
@@ -2,30 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state= {
-      value: null
-    }
-  }
+// The Square components are "controlled components". The Board component controls what happens to them.
+class Square extends React.Component {
 
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}>
+        onClick={() => this.props.onClick()}>
         {/* TODO */}
-      {this.state.value}
+      {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({
+      squares: squares
+    });
+  }
+
+  // What is i?
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
